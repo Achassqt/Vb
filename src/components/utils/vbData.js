2fdf203bb4c354c24c43a61167cb6beade07b11c
Achassqt <sachsqt@gmail.com>
2024-01-08 13:37:41 +0100

    refactor: Modify point placement function to support array of objects

---
+++ src/components/utils/vbData.js
@@ -0,0 +1,14 @@
+export const vbData = [
+  {
+    region: "region-2",
+    name: "VB Bègles",
+    longitude: -0.5298133,
+    latitude: 44.7971999,
+  },
+  {
+    region: "region-2",
+    name: "VB Bordeaux-Lac",
+    longitude: -0.561259,
+    latitude: 44.8734289,
+  },
+];
